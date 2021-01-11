3411d13bc9e8e96dffb790f49eb6e78692dfcb6d
Max Lima <max.lima2@gmail.com>
2021-01-11 13:31:12 -0300

    change variables name to improve legibility

--- Buddy Pairs/solution.dart
+++ Buddy Pairs/solution.dart
@@ -2,12 +2,12 @@ int getDivisorsSum(int x) => List.generate(x ~/ 2, (n) => n + 1)
     .where((n) => x % n == 0)
     .reduce((x, y) => x + y);
 
-List<int> buddy(int a, int b) {
-  if (a < b) {
-    for (int i = a; i <= b; i++) {
+List<int> buddy(int start, int limit) {
+  if (start < limit) {
+    for (int i = start; i <= limit; i++) {
       final sum = getDivisorsSum(i);
 
-      if (sum > 1 && getDivisorsSum(sum - 1) - 1 == i) {
+      if (sum > 1 && (getDivisorsSum(sum - 1) - 1 == i)) {
         return [i, sum - 1];
       }
     }
